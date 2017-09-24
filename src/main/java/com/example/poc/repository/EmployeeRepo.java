c71fb16513e034f118974f22ce5939e3e6814882
Vinay Kumar <vinaykumar.p@live.com>
2017-09-24 11:16:34 +0530

    Chaged native query to HQL

--- src/main/java/com/example/poc/repository/EmployeeRepo.java
+++ src/main/java/com/example/poc/repository/EmployeeRepo.java
@@ -1,25 +1,23 @@
 package com.example.poc.repository;
 
-import java.util.List;
-import java.util.Optional;
-
+import com.example.poc.dto.EmpProj;
+import com.example.poc.entity.Employee;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.example.poc.dto.EmpDto;
-import com.example.poc.dto.EmpProj;
-import com.example.poc.entity.Employee;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepo extends CrudRepository<Employee, Long> {
 
-	@Query(value="select * from employee where id=?1",nativeQuery=true)
-	Optional<Employee> findById(Long id);
-	
-	
-	@Query(value="select e.empname as empName, d.depname from employee as e inner Join Department as d on e.id = d.id",nativeQuery=true)
-	List<EmpProj> findById1();
-	
-	
+    @Query(value = "select * from employee where id=?1", nativeQuery = true)
+    Optional<Employee> findById(Long id);
+
+
+    @Query(value = "select e.empName as empName, d.depName as depName from Employee e inner join Department as d on e.id = d.id", nativeQuery = false)
+    List<EmpProj> findById1();
+
+
 }
